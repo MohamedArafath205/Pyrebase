e1191bbf0a14c39df4f5186d0707088606288ad8
Mohamed Arafath <113768609+MohamedArafath205@users.noreply.github.com>
2023-02-25 16:24:23 +0530

    Add files via upload

---
+++ Pyrebase.py
@@ -0,0 +1,9 @@
+firebaseConfig = {
+  apiKey: "AIzaSyB90Ka0pHy0SNuSqjt8cM_E81J2dSQ5E2k",
+  authDomain: "pyrebase-ddfca.firebaseapp.com",
+  projectId: "pyrebase-ddfca",
+  storageBucket: "pyrebase-ddfca.appspot.com",
+  messagingSenderId: "747491423505",
+  appId: "1:747491423505:web:03d236a30305c4880e48b0",
+  measurementId: "G-TVBWK7Z3Y1"
+};
